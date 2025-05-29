5f55d5a79f1a3aaedf33e3beec813dfff517f534
GakuruAlex <gakurualexgicimo@gmail.com>
2025-05-30 00:22:06 +0300

    collection expression

--- Program.cs
+++ Program.cs
@@ -1,9 +1,4 @@
-﻿string[] fraudulentOrderIDs = new string[3];
-
-fraudulentOrderIDs[0] = "A123";
-fraudulentOrderIDs[1] = "B456";
-fraudulentOrderIDs[2] = "C789";
-
+﻿string[] fraudulentOrderIDs = ["A123", "B456", "C789"];
 
 Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
 Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
