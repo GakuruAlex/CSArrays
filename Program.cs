c23cb7056227443de7db2b7d0291c45ef232d97a
GakuruAlex <gakurualexgicimo@gmail.com>
2025-05-30 00:28:22 +0300

    using foreach

--- Program.cs
+++ Program.cs
@@ -1,7 +1,18 @@
-﻿string[] fraudulentOrderIDs = ["A123", "B456", "C789"];
+﻿using System.Runtime.CompilerServices;
+
+string[] fraudulentOrderIDs = ["A123", "B456", "C789"];
 
 Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
 Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
 Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
 
 Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
+
+int[] inventory = { 200, 450, 700, 175, 250 };
+int total = 0;
+foreach (int item in inventory)
+{
+    total += item;
+}
+
+Console.WriteLine($"Total: {total}");
